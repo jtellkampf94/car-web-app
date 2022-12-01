83b94d5606471067c3b7daffb2b873d1d7ecb7a7
Jonathan <jtellkampf@gmail.com>
2022-12-01 18:42:31 +0000

    Created Cars component and started topCars component

---
+++ src/app/containers/HomePage/topCars.tsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+import { ICar } from "../../../typings/car";
+import Car from "../../components/car";
+import Carousel, { Dots, slidesToShowPlugin } from "@brainhubeu/react-carousel";
+import "@brainhubeu/react-carousel/lib/style.css";
+import { SCREENS } from "../../components/responsive";
+
+const TopCarsContainer = styled.div`
+  ${tw`
+    max-w-screen-lg
+    w-full
+    flex
+    flex-col
+    items-center
+    justify-center
+    pr-4
+    pl-4
+    md:pl-0
+    md:pr-0
+    mb-10
+  `};
+`;
+
+const Title = styled.h2`
+  ${tw`
+    text-3xl
+    lg:text-5xl
+    text-black
+    font-extrabold
+  `};
+`;
+
+const CarsContainer = styled.div`
+  ${tw`
+    w-full
+    flex
+    flex-wrap
+    justify-center
+    mt-7
+    md:mt-10
+  `};
+`;
+
+const EmptyCars = styled.div`
+  ${tw`
+    w-full
+    flex
+    justify-center
+    items-center
+    text-sm
+    text-gray-500
+  `};
+`;
+
+const LoadingContainer = styled.div`
+  ${tw`
+    w-full
+    mt-9
+    flex
+    justify-center
+    items-center
+    text-base
+    text-black
+  `};
+`;
+
+const TopCars: React.FC = () => {
+  return <div></div>;
+};
+
+export default TopCars;
